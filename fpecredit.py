81f7ecbe3f4bff9fade27259b9304db551369d1a
giorgos314 <pipilisg@gmail.com>
2018-02-10 03:57:44 +0200

    AES-128 encryption now works for a single round

--- fpecredit.py
+++ fpecredit.py
@@ -12,21 +12,30 @@ else:
     if len(repr(cardnum)) != 16:
         raise ValueError("Credit Card number must have exactly 16 digits")
 
+# Test value, TODO: multiple rounds
+round = 1
+
 # Turn the card number into a binary number with exactly 54 digits
 cardnum = format(cardnum, '054b')
 # Split the number into two 27-bit numbers
 left = cardnum[:27]
 right = cardnum[27:]
 
+# This is the AES encryption used for each round in the Feistel network
 # We use ECB with no problem since we encrypt only one block with it
-def encrypt(cardnum, key):
+def aes_enc(cardnum, round):
+    # Each round we get the AES-128 key by padding the initial right number with
+    # zeroes and appending the 8-bit, binary representation of the round number
+    key = right.zfill(120) + format(round, '08b')
+
     encrypter = AS.new(key, AES.MODE_ECB)
     cardnum = encrypter.encrypt(cardnum)
 
     return cardnum;
 
+# This is the AES decryption used for each round in the Feistel network
 # For decryption we just need to reverse the encryption key schedule
-def decrypt(cardnum, key):
+def aed_dec(cardnum, round):
     decrypter = AES.new(key, AES.MODE_ECB)
     cardnum = decrypter.decrypt(cardnum)
 
