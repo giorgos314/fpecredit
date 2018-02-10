d0c3fac44b2ab812993a7749bc1c390c2fec0862
giorgos314 <pipilisg@gmail.com>
2018-02-10 03:32:01 +0200

    Added basic input manipulation that prepares data for the Feistel Network

--- fpecredit.py
+++ fpecredit.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
-
 from Crypto.Cipher import AES
+
+# Credit card numbers must be of type int
+try:
+    cardnum = int(input("What is the card number?"))
+except ValueError as err:
+    print("Credit Card number must be a number!\n", err)
+    raise
+else:
+    # Credit cards must have 16 digits
+    if len(repr(cardnum)) != 16:
+        raise ValueError("Credit Card number must have exactly 16 digits")
+
+# Turn the card number into a binary number with exactly 54 digits
+cardnum = format(cardnum, '054b')
+# Split the number into two 27-bit numbers
+left = cardnum[:27]
+right = cardnum[27:]
 
 # We use ECB with no problem since we encrypt only one block with it
 def encrypt(cardnum, key):
