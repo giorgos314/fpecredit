e284613d733ec26ff5f5db7629a5e0228ed2539d
giorgos314 <pipilisg@gmail.com>
2018-02-23 08:59:27 +0200

    AES encryption now returns a 27 digit bitstring

--- fpecredit.py
+++ fpecredit.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from bitstring import Bits
 
 # Credit card numbers must be of type int
 try:
@@ -13,30 +15,38 @@ else:
         raise ValueError("Credit Card number must have exactly 16 digits")
 
 # Test value, TODO: multiple rounds
-round = 1
+round = Bits(int = 1, length = 101)
 
 # Turn the card number into a binary number with exactly 54 digits
-cardnum = format(cardnum, '054b')
+cardnum = Bits(int = cardnum, length = 54)
+
 # Split the number into two 27-bit numbers
 left = cardnum[:27]
 right = cardnum[27:]
 
+# bin > SHA256 > hexdigest
+
 # This is the AES encryption used for each round in the Feistel network
 # We use ECB with no problem since we encrypt only one block with it
 def aes_enc(cardnum, round):
-    # Each round we get the AES-128 key by padding the initial right number with
+    # TODO: Each round we get the AES-128 key by padding the initial right number with
     # zeroes and appending the 8-bit, binary representation of the round number
-    key = right.zfill(120) + format(round, '08b')
 
-    encrypter = AS.new(key, AES.MODE_ECB)
-    cardnum = encrypter.encrypt(cardnum)
+    key = cardnum + round
+    print(key)
+    print(key.hex)
+    print(len(key))
 
-    return cardnum;
+    encrypter = AES.new(key.hex, AES.MODE_ECB)
+    output = encrypter.encrypt(Bits().join([cardnum, '0b00000']).bin)
+    output = Bits(output)[:27]
+
+    return output
 
 # This is the AES decryption used for each round in the Feistel network
 # For decryption we just need to reverse the encryption key schedule
-def aed_dec(cardnum, round):
+def aes_dec(cardnum, round):
     decrypter = AES.new(key, AES.MODE_ECB)
-    cardnum = decrypter.decrypt(cardnum)
+    cardnum = decrypter.decrypt(key)
 
-    return cardnum;
+    return cardnum
